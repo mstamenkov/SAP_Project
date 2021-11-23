eb601473d0f55a9e8f15d0f7bb4cee90d9c55f7f
Martin Stamenkov <bul2013@abv.bg>
2021-11-23 15:46:59 +0200

    initial spring project structure

---
+++ SAP_Project/src/main/java/com/example/sap_project/SapProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.sap_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SapProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SapProjectApplication.class, args);
+    }
+
+}
