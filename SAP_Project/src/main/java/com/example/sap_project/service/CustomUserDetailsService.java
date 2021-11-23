ec9f9a5acb477a5d27d2c3fa7bef980185ef71ca
Martin Stamenkov <bul2013@abv.bg>
2021-11-24 01:03:36 +0200

    basic login implemented

---
+++ SAP_Project/src/main/java/com/example/sap_project/service/CustomUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.example.sap_project.service;
+
+import com.example.sap_project.model.User;
+import com.example.sap_project.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepo.findByEmail(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        return new CustomUserDetails(user);
+    }
+
+}
